12538031111fe173a9cfd987b7e5b53ad5a99c1e
playerzer0-ui <playerzero745@gmail.com>
2024-03-04 15:02:44 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Jeremiah_SupermarketOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
